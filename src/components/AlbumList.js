c9bbf70c3270f8d9cdaa1bc595b1a3742cfbc189
Luka Bratos <luka.bratos@gmail.com>
2017-04-14 20:55:45 +0100

    Simple network call to the API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,6 +4,9 @@ import { View, Text } from 'react-native';
 class AlbumList extends Component {
     componentWillMount() {
         console.log('componentWillMount in AlbumList')
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response.json()))
+        .catch(error => console.log(error))
     }
 
     render () {
