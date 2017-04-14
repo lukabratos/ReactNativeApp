99b3b5ffdc2eadb7cdc07104e53a7d96fc0bb580
Luka Bratos <luka.bratos@gmail.com>
2017-04-14 18:49:48 +0100

    Add shadow to the bottom line

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,10 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
-        paddingTop: 15
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2
     },
     textStyle: {
         fontSize: 20
