b0bca9041ea998c443d216c130439b0c30a72fd4
Luka Bratos <luka.bratos@gmail.com>
2017-04-15 11:38:00 +0100

    Pass albums title to the card through props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View } from 'react-native'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <View style={styles.containerStyle}>
+            {props.children}
         </View>
     )
 }
