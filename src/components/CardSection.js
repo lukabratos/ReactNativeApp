0091f7c2cdfdf2a47d057b51eaa5dd71d7144520
Luka Bratos <luka.bratos@gmail.com>
2017-04-15 11:45:00 +0100

    Add styling to the CardSection component

---
+++ src/components/CardSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const CardSection = () => {
+    return (
+        <View style={styles.containerStyle}>
+        </View>
+    )
+}
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#FFF',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#DDD',
+        position: relative
+    }
+}
+
+export default CardSection
