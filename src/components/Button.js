b567f329ffa633b598a5f3db5a425da85ab61567
Luka Bratos <luka.bratos@gmail.com>
2017-04-15 14:17:36 +0100

    Add button-like behaviour to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 
 const Button = () => {
     return (
-        <Text>Click me!!!</Text>
+        <TouchableOpacity>
+            <Text>Click me!!!</Text>
+        </TouchableOpacity>
     )
 }
 
