0aa460fe8a0f01524828c6c8535839df7c378bd7
Luka Bratos <luka.bratos@gmail.com>
2017-04-15 13:40:17 +0100

    Increase title font size and add padding to the thumbnail image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,19 +6,19 @@ import CardSection from './CardSection'
 const AlbumDetail = ({ album }) => {
 
     const { title, artist, thumbnail_image } = album
-    const { thumbnailStyle, headerContentStyle } = styles
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle } = styles
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={ thumbnailStyle }
                         source={{ uri: thumbnail_image }} 
                     />
                 </View>
                 <View style={ headerContentStyle }>
-                    <Text>{ title }</Text>
+                    <Text style={headerTextStyle}>{ title }</Text>
                     <Text>{ artist }</Text>
                 </View>
             </CardSection>
@@ -31,9 +31,18 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 }
 
