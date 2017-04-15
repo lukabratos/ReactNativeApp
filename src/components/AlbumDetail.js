95312781b3459aa12efc6db19f39f4faac7ee188
Luka Bratos <luka.bratos@gmail.com>
2017-04-15 12:09:58 +0100

    Destructure props

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+    const { title, artist, thumbnail_image } = album
+
     return (
         <Card>
             <CardSection>
-                <View></View>
-                <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                <View>
+                    <Image source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={ styles.headerContentStyle }>
+                    <Text>{ title }</Text>
+                    <Text>{ artist }</Text>
                 </View>
             </CardSection>
         </Card>
